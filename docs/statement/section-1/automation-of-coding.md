20086eb38e02f5fff0e97cf50b384146150aef0d
otaka44 <332953@gmail.com>
2026-02-10 00:08:34 +0900

    first commit

---
+++ docs/statement/section-1/automation-of-coding.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+title: 第1節.コーディング、デバッグ、デプロイの自動化
+---
+Automation of Coding, Debugging, and Deployment
+
+AIエージェントが開発の主導権を握る世界： コーディング、デバッグ、デプロイの自動化。
---
+++ docusaurus.config.ts
@@ -0,0 +1,134 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "砂場録宣言'26",
+  tagline: "sunabalog statement '26",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://sunaba-log.github.io",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "sunaba-log", // Usually your GitHub org/user name.
+  projectName: "statement", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+
+  onBrokenLinks: "throw",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "ja",
+    locales: ["ja", "en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/sunaba-log/sunabalog-statement/tree/main/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "砂場録",
+      logo: {
+        alt: "砂場録 Logo",
+        src: "img/dotted_brain.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "statementSidebar",
+          position: "left",
+          label: "宣言",
+        },
+        // { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/sunaba-log",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "宣言",
+              to: "/docs/statement/intro",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/sunaba-log",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sunabalog, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
