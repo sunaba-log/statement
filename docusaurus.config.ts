0f710dc8ab0fbf45b3bbab4e591ee2b710482937
otaka44 <332953@gmail.com>
2026-02-11 02:14:24 +0900

    update editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,8 +43,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/sunaba-log/sunabalog-statement/tree/main/",
+          editUrl: "https://github.com/sunaba-log/statement/tree/main/",
         },
         blog: {
           showReadingTime: true,
@@ -54,8 +53,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/sunaba-log/statement/tree/main/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
